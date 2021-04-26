58e65c8160d127cf73a38566def03257c3efcd0b
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-04-26 23:28:39 +0300

    Heroku settings

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y6ncmp0^u9dx-=7@%x^%i=dy+aw7)q_5vex^pfxmkhsiihijl#
 DEBUG = False
 # Added for the bootrstrap
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-ALLOWED_HOSTS = ['blogpost-ingressive4good.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1', '.blogpost-ingressive4good.herokuapp.com']
 
 
 # Application definition
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# added
+from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
