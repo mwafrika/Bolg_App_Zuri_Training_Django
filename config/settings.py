44874085ece41229ac2dac3dd4b9cc7528627a16
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-04-26 18:11:09 +0300

    Change the procfile

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +136,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'josuemwafrika2020@gmail.com'
 EMAIL_HOST_PASSWORD = 'josmwafrika789'
+
+django_heroku.settings(locals())
