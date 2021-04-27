381569caf2d33efdb9b1e805e2be285034a3cad9
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-04-27 12:05:07 +0300

    PIP env activation

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # added
-from dj_static import Cling
+# from dj_static import Cling
 # from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
